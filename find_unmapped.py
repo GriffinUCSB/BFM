07a39d4b72751f8e083178ed343ef0fd95693b3d
GriffinUCSB <111609895+GriffinUCSB@users.noreply.github.com>
2022-09-02 11:41:30 -0700

    Add files via upload
    version update to find_unmapped.py that makes the program user-friendly. Adds argsparse to allow for command-line user input, removes pesky warnings, adds optional percentage tracker

--- find_unmapped.py
+++ find_unmapped.py
@@ -9,11 +9,26 @@
 #imports
 import csv, pandas as pd
 import os
+import argparse
+import warnings
+import sys
 
+warnings.filterwarnings("ignore")
+
+parser = argparse.ArgumentParser(description='proccess user in')
+parser.add_argument("--header", help="add header to output file", action="store_true")
+parser.add_argument('-p', "--percentage", help="print complete percentsge", action="store_true")
+parser.add_argument('-i', "--include_unfound", help="include reads that remain unfound in output", action="store_true")
+parser.add_argument('-n', '--file_names',nargs='+', default=[] ,help='Input.txt file names in this order: unmapped_reads.txt, output.txt, libfile1.txt, libfileN.txt')
+args=parser.parse_args()
 #variables for configuration
-unmapped_file_name = "fixedfinalUnmapped.txt"
-library_file_names = ["urLib1.txt", "urLib2.txt"]
-header = True #If this program is being used to create a new .csv, set header to True in config
+unmapped_file_name = args.file_names[0]
+library_file_names = args.file_names[2:]
+
+if args.header:
+    header = True #If this program is being used to create a new .csv, set header to True in config
+else:
+    header = False
 
 def search_libraries(unmapped, library, header):
 
@@ -31,7 +46,7 @@ def search_libraries(unmapped, library, header):
     for library_iterator in range (len(library)):
         librarydfs.append(pd.read_csv(library[library_iterator], header = 0, delimiter="\t"))
 
-    #search
+    #search rowUnmapped
     for unmappedDF_iterator in range (rowUnmapped):
         found = False
         for library_list_iterator in range (len(librarydfs)):
@@ -41,12 +56,15 @@ def search_libraries(unmapped, library, header):
                     found = True
                     temp = pd.DataFrame([[librarydfs[library_list_iterator].at[library_df_iterator, "sgId"], unmappeddf.at[unmappedDF_iterator, "seq"], unmapped, unmappeddf.at[unmappedDF_iterator, "freq"], library[library_list_iterator]]], columns=output_columns)
                     outdf = outdf.append(temp)
-        #if (found == False):
-        #    temp = pd.DataFrame([["N/A", unmappeddf.at[unmappedDF_iterator, "seq"], unmapped, unmappeddf.at[unmappedDF_iterator, "freq"], "N/A"]], columns=output_columns)
-        #    outdf = outdf.append(temp)
+        if (found == False and args.include_unfound):
+            temp = pd.DataFrame([["N/A", unmappeddf.at[unmappedDF_iterator, "seq"], unmapped, unmappeddf.at[unmappedDF_iterator, "freq"], "N/A"]], columns=output_columns)
+            outdf = outdf.append(temp)
+        if args.percentage:
+            sys.stdout.write('\r')
+            sys.stdout.write("Percentage complete: " + str(unmappedDF_iterator/rowUnmapped * 100))
 
     #send output to csv
-    outdf.to_csv('foundUnmapped3.txt', sep="\t", mode='a', index=False, header=header)
+    outdf.to_csv(args.file_names[1], sep="\t", mode='a', index=False, header=header)
 
 #call function
 search_libraries(unmapped_file_name, library_file_names, header)
