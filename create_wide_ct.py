8f1c71076cb4ea87de34aba668ea04b843e42665
GriffinUCSB <111609895+GriffinUCSB@users.noreply.github.com>
2022-09-27 19:34:12 -0700

    Add files via upload

--- create_wide_ct.py
+++ create_wide_ct.py
@@ -5,11 +5,20 @@
 #imports
 import csv, pandas as pd
 import os
+import argparse
 
-#input count file names here
-count_file_names = ["B1-T0.sort.txt", "B1-Un-T14.sort.txt", "B1-Z-T14.sort.txt", "B2-T0.sort.txt", "B2-Un-T14.sort.txt", "B2-Z-T14.sort.txt"]
-header = True #If this program is being used to create a new .csv, set header to True in config
+parser = argparse.ArgumentParser(description='proccess user in')
+parser.add_argument("--header", help="add header to output file", action="store_true")
+parser.add_argument('-n', '--file_names',nargs='+', default=[] ,help='Input.txt file names in this order: output.txt CountTable1.txt ... CountTableN.txt')
+args=parser.parse_args()
 
+count_file_names = args.file_names[1:]
+
+#count_file_names = ["B1-T0.sort.txt", "B1-Un-T14.sort.txt", "B1-Z-T14.sort.txt", "B2-T0.sort.txt", "B2-Un-T14.sort.txt", "B2-Z-T14.sort.txt"]
+if args.header:
+    header = True #If this program is being used to create a new .csv, set header to True in config
+else:
+    header = False
 #convert from sgID to gene name
 def get_gene_name(sgID):
     if ("neg" in sgID):
@@ -40,7 +49,7 @@ def create_wide_count_table(names, header):
         temp_df = pd.read_csv(names[k+1], header = 0, delimiter="\t")
         temp_df.columns = ['sgRNA', names[k+1]]
         tbl = tbl.merge(temp_df, on='sgRNA')
-        print(str(k)+"/"+str(len(names)))
+
 
     #make file compatible for MAGeCK INC
     temp_array = []
@@ -51,7 +60,7 @@ def create_wide_count_table(names, header):
     #for i in range(rows-1):
     #    tbl.at[i, "gene"] = get_gene_name(tbl.at[i, "sgRNA"])
 
-    outdf = outdf.append(tbl) 
-    outdf.to_csv('PEX_Screen_wct.txt', sep="\t", mode='a', index=False, header=header)
+    outdf = outdf.append(tbl)
+    outdf.to_csv(args.file_names[0], sep="\t", mode='a', index=False, header=header)
 
 create_wide_count_table(count_file_names, header)
